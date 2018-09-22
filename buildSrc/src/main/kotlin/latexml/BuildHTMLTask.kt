a2daec1ab16e60d389c364f41b6e90c7effda033
Alexander Nozik <altavir@gmail.com>
2018-09-22 10:11:24 +0300

    Adjustments to buildsrc. Preparing for standalone module.

--- buildSrc/src/main/kotlin/latexml/BuildHTMLTask.kt
+++ buildSrc/src/main/kotlin/latexml/BuildHTMLTask.kt
@@ -13,11 +13,20 @@ open class BuildHTMLTask : DefaultTask() {
     @Option(description = "Source file name or template without `.tex` suffix")
     lateinit var source: String
 
-    @Option(description = "the name of the target xml/html")
-    var target: String? = null
+    @Option(description = "the name of the target xml/html (without extension)")
+    var target: String = "index"
 
+    @Option(description = "If not empty, use target tex file as a template and substitute strings with this parameters using groovy templete engine")
     var parameters: Map<String, String> = emptyMap()
 
+    @Option(description = "List of js imports")
+    var js: MutableList<String> = arrayListOf("http://fred-wang.github.io/mathjax.js/mpadded-min.js")
+
+    @Option(description = "List of css imports")
+    var css: MutableList<String> = ArrayList()
+
+
+    @OutputDirectory
     val logDir = File(project.buildDir, "logs")
 
     @OutputDirectory
@@ -38,32 +47,38 @@ open class BuildHTMLTask : DefaultTask() {
         logDir.mkdirs()
         xmlDir.mkdirs()
 
-        val xmlLine = project.platformCommand + listOf("latexml",
-                "--destination=\"${convertPath(project, xmlFile.absolutePath)}\"",
-                "--inputencoding=utf8",
+
+        val xmlLine = project.platformCommand +
+                listOf(
+                        "latexml",
+                        "--destination=\"${convertPath(project, xmlFile.absolutePath)}\"",
+                        "--inputencoding=utf8",
 //                "--includestyles",
-                "\"${convertPath(project, sourceFile.absolutePath)}\""
-        )
+                        "\"${convertPath(project, sourceFile.absolutePath)}\""
+                )
 
         logger.lifecycle("Preparing xml")
         logger.lifecycle("Using command line for latexml $target: ${xmlLine.joinToString(" ")}")
         project.exec {
             workingDir = project.rootDir
-            errorOutput = FileOutputStream(File(logDir, "${target}.xml.log"))
+            errorOutput = FileOutputStream(File(logDir, "$target.xml.log"))
             //TODO убрать первый аргумент для линукса
             commandLine = xmlLine
         }
 
         htmlDir.mkdirs()
 
-        val htmlLine =  project.platformCommand + listOf(
+        val jsArray = js.map { "--javascript='$it'" }.toTypedArray()
+        val cssArray = css.map { "--css='$it'" }.toTypedArray()
+
+        val htmlLine = project.platformCommand + listOf(
                 "latexmlpost",
-                "--destination=${convertPath(project, htmlDir.toString())}/index.html",
+                "--destination=${convertPath(project, htmlDir.toString())}/$target.html",
                 "--format=html5",
 //                "--split",
 //                "--splitat=section",
-                "--javascript='http://fred-wang.github.io/mathjax.js/mpadded-min.js'",
-//                "--css=labnik.css",
+                *jsArray,
+                *cssArray,
                 convertPath(project, xmlFile.absolutePath)
         )
 
@@ -71,7 +86,7 @@ open class BuildHTMLTask : DefaultTask() {
 
         project.exec {
             workingDir = project.rootDir
-            errorOutput = FileOutputStream(File(logDir, "${target}.html.log"))
+            errorOutput = FileOutputStream(File(logDir, "$target.html.log"))
             commandLine = htmlLine
 
         }
